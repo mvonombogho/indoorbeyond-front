0607ca16892344576d97f6ffa4c46a6dbefce9a1
Israel Mvono <israrlmvono@gmail.com>
2024-02-20 01:13:32 +0300

    key update

--- components/Tabs.js
+++ components/Tabs.js
@@ -21,6 +21,7 @@ export default function Tabs({tabs,active,onChange}) {
     <StyledTabs>
       {tabs.map(tabName => (
         <StyledTab
+        key={tabName.id}
           onClick={() => { onChange(tabName) }}
           active={tabName === active}
         >{tabName}</StyledTab>
